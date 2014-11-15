da725aaa68779966bc5b21e7237f110d250e7305
Giancarlo <orion2014@users.noreply.github.com>
2014-11-15 12:07:58 +0100

    pollutantmean
    added id, fixed "path_vector" missing slash

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,5 +1,6 @@
-pollutantmean <- function(directory) {
+pollutantmean <- function(directory,id) {
 directory_path <- paste(getwd(),"/",directory, sep="")
-file_path_vector <- paste(directory_path, as.character(list.files(directory_path)), sep="")
-list(file_path_vector)
+file_path_vector <- paste(directory_path, "/", as.character(list.files(directory_path)), sep="")
+frame <- read.csv(file_path_vector[id])
+list(frame)
 }
