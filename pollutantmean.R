74c12068c4771d14f37be6f0b5f3e714f88b4752
Giancarlo <orion2014@users.noreply.github.com>
2014-11-15 14:53:53 +0100

    add pollutant
    data vector stores pollutant data

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,8 +1,9 @@
-pollutantmean <- function(directory,id) {
+pollutantmean <- function(directory,pollutant,id) {
 directory_path <- paste(getwd(),"/",directory, sep="")
 file_path_vector <- paste(directory_path, "/", as.character(list.files(directory_path)), sep="")
 data_vector <- vector(mode = "numeric", length = 0) ## define vector to store the values
 for (i in id) { ## previous version could read only 1 id, but not a range
     frame <- read.csv(file_path_vector[i],header=T)
-    }
+    data_vector <-c(data_vector,frame[[pollutant]]) ## store pollutant data in vector
+	}
 }
