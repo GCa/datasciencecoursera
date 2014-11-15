84d55bca12822940c373fe5ec6a9f1145e5cb557
Giancarlo <orion2014@users.noreply.github.com>
2014-11-15 12:58:51 +0100

    pollutantmean
    added for loop, to read a range of IDs

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,6 +1,7 @@
 pollutantmean <- function(directory,id) {
 directory_path <- paste(getwd(),"/",directory, sep="")
 file_path_vector <- paste(directory_path, "/", as.character(list.files(directory_path)), sep="")
-frame <- read.csv(file_path_vector[id])
-list(frame)
+for (i in id) { ## previous version could read only 1 id, but not a range
+    frame <- read.csv(file_path_vector[i],header=T)
+    }
 }
