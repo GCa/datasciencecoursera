7a291e4727329bebb3eeae6e8b82e96fb38d7bc7
Giancarlo <orion2014@users.noreply.github.com>
2014-11-15 15:19:12 +0100

    fix for default value
    missed default value

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,4 +1,4 @@
-pollutantmean <- function(directory,pollutant,id) {
+pollutantmean <- function(directory,pollutant,id = 1:332) {
 directory_path <- paste(getwd(),"/",directory, sep="")
 file_path_vector <- paste(directory_path, "/", as.character(list.files(directory_path)), sep="")
 data_vector <- vector(mode = "numeric", length = 0) ## define vector to store the values
